e9c1c81a098bc551f7749b780c7449dfcdcfa8d2
Krista Dotzenrod <kdotzenrod517@gmail.com>
2022-04-25 17:34:01 -0500

    Longest prefix

---
+++ src/LongestPrefix.java
@@ -0,0 +1,27 @@
+public class LongestPrefix {
+
+    public static void main(String[] args) {
+        String[] col = new String[]{"colorado", "color", "cold"};
+        String[] none = new String[]{"a", "b", "c"};
+        String[] spot = new String[]{"spot", "spotty", "spotted"};
+        System.out.println(prefix(col));
+        System.out.println(prefix(none));
+        System.out.println(prefix(spot));
+    }
+    public static String prefix(String[] strs) {
+
+        if(strs.length == 0) return "";
+        String prefix = strs[0];
+
+        for(int i = 1; i < strs.length; i++){
+            while(strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if(prefix.isEmpty()) return "";
+            }
+        }
+
+        return prefix;
+    }
+}
+
+
