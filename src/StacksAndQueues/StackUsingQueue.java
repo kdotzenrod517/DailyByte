c49097a226163295853e3d171efd639e69faab51
Krista Dotzenrod <kdotzenrod517@gmail.com>
2022-05-19 16:52:03 -0500

    Stack using queue

---
+++ src/StacksAndQueues/StackUsingQueue.java
@@ -0,0 +1,41 @@
+package StacksAndQueues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+// Design a class to implement a stack using only a single queue.
+public class StackUsingQueue {
+
+    private Queue<Integer> q1 = new LinkedList<>();
+    private Queue<Integer> q2 = new LinkedList<>();
+    private int top;
+
+
+    // Push element x onto stack.
+    public void push(int x) {
+        q1.add(x);
+        top = x;
+    }
+
+    // Removes the element on top of the stack.
+    public void pop() {
+        while (q1.size() > 1) {
+            top = q1.remove();
+            q2.add(top);
+        }
+        q1.remove();
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+
+    // Get the top element.
+    public int top() {
+        return top;
+    }
+
+    // Return whether the stack is empty.
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
