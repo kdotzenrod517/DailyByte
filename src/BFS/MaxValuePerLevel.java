259b73998589e5bb533adcf2b54cd0ae4487ad8a
Krista Dotzenrod <kdotzenrod517@gmail.com>
2022-05-27 09:50:36 -0500

    Max value per level

---
+++ src/BFS/MaxValuePerLevel.java
@@ -0,0 +1,50 @@
+package BFS;
+
+import utils.Node;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import static utils.CreateBST.getBST;
+
+public class MaxValuePerLevel {
+
+    public static void main(String[] args){
+        System.out.println(maxValueInLevels(getBST()));
+    }
+
+    public static List<Integer> maxValueInLevels(Node root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) {
+            return result;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        int size = 1;
+
+        while(!queue.isEmpty()) {
+
+            int max = Integer.MIN_VALUE;
+
+            for(int i = 0; i < size; i++) {
+                Node node = queue.remove();
+                max = Math.max(max, node.data);
+
+                if(node.left != null) {
+                    queue.add(node.left);
+                }
+                if(node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+
+            size = queue.size();
+            result.add(max);
+        }
+
+        return result;
+    }
+}
