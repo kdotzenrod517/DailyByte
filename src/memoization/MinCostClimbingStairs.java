3ab5ec180ec66cad311fbc3da9e87a2c219ee15e
Krista Dotzenrod <kdotzenrod517@gmail.com>
2022-06-24 14:29:04 -0500

    Min cost

--- src/memoization/MinCostClimbingStairs.java
+++ src/memoization/MinCostClimbingStairs.java
@@ -2,33 +2,29 @@ package memoization;
 
 public class MinCostClimbingStairs {
 
-    static int[] memo;
-
     public static void main(String[] args) {
         int[] cost1 = {5, 10, 20};
         int[] cost2 = {1, 5, 10, 3, 7, 2};
-        System.out.println(climbingStairs(cost1)); // 10
-        System.out.println(climbingStairs(cost2)); // 10
+        System.out.println(minCostStairs(cost1)); // 10
+        System.out.println(minCostStairs(cost2)); // 10
     }
 
-    public static int climbingStairs(int[] cost) {
-        memo = new int[cost.length];
-        minCost(cost, 0);
-        return Math.min(memo[0], memo[1]);
+    // Runtime: O(N) where N is the total number of elements in cost.
+    // Space complexity: O(N) where N is the total number of elements in cost. This extra space results in our memoize array.
+    public static int minCostStairs(int[] cost) {
+        int[] memoize = new int[cost.length];
+        return Math.min(climbSteps(cost.length - 1, cost, memoize), climbSteps(cost.length - 2, cost, memoize));
     }
 
-    private static int minCost(int[] cost, int i) {
-        if (i == cost.length - 1) {
-            return cost[i];
-        } else if (i >= cost.length) {
-            return 0;
+    public static int climbSteps(int step, int[] cost, int[] memoize) {
+        if (step == 0 || step == 1) {
+            return cost[step];
+        } else if (memoize[step] != 0) {
+            return memoize[step];
+        } else {
+            memoize[step] = cost[step] + Math.min(climbSteps(step - 1, cost, memoize),
+                    climbSteps(step - 2, cost, memoize));
+            return memoize[step];
         }
-        if (memo[i] == 0) {
-            int step1 = minCost(cost, i + 1) + cost[i];
-            int step2 = minCost(cost, i + 2) + cost[i];
-            memo[i] = Math.min(step1, step2);
-        }
-        return memo[i];
     }
-
 }
