591fea89e5ee26380901711434e18ac49862ff84
Krista Dotzenrod <kdotzenrod517@gmail.com>
2022-07-05 15:43:23 -0500

    Longest increasing subsequence

---
+++ src/dynamicProgramming/DictionaryWords.java
@@ -0,0 +1,28 @@
+package dynamicProgramming;
+
+import java.util.Set;
+
+public class DictionaryWords {
+
+    public static void main(String[] args){
+        System.out.println(wordBreak("thedailybyte", Set.of("the", "daily", "byte"))); // true
+        System.out.println(wordBreak("pizzaplanet", Set.of("plane", "pizza"))); // false
+    }
+
+    public static boolean wordBreak(String s, Set<String> dict) {
+
+        boolean[] f = new boolean[s.length() + 1];
+        f[0] = true;
+
+        for(int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (f[j] && dict.contains(s.substring(j, i))) {
+                    f[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return f[s.length()];
+    }
+}
---
+++ src/dynamicProgramming/LongestIncreasingSubsequence.java
@@ -0,0 +1,32 @@
+package dynamicProgramming;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequence {
+
+    public static void main(String[] args) {
+        System.out.println(longestIncreasingSubsequence(new int[]{1, 9, 7, 4, 7, 13})); // 4, The longest increasing subsequence is 1, 4, 7, 13.
+    }
+
+    // Runtime: O(N2) where N is the number of elements in nums. This results from having our two nested for loops.
+    // Space complexity: O(N) where N is the number of elements in nums. This results from storing N elements in our dp array.
+    public static int longestIncreasingSubsequence(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        for(int i = 1; i < dp.length; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int longest = Integer.MIN_VALUE;
+        for(int i: dp) {
+            longest = Math.max(longest, i);
+        }
+
+        return longest;
+    }
+}
