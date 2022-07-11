82e9d32b73b7fdb055016b8a07da77440b246bb8
Krista Dotzenrod <kdotzenrod517@gmail.com>
2022-07-11 18:44:20 -0500

    Create trie

---
+++ src/AdvancedTopics/CreateTrie.java
@@ -0,0 +1,54 @@
+package AdvancedTopics;
+
+public class CreateTrie {
+
+    public static void main(String[] args){
+        Trie trie = new Trie();
+        trie.insert("programming");
+        System.out.println(trie.search("computer")); // false
+        System.out.println(trie.search("programming")); // true
+    }
+
+    static class Node {
+        public Node[] children;
+        public String word;
+
+        public Node() {
+            children = new Node[26];
+            word = "";
+        }
+    }
+
+    public static class Trie {
+        private Node root;
+
+        public Trie() {
+            root = new Node();
+        }
+
+        public void insert(String word) {
+            Node current = root;
+            for (char c : word.toCharArray()) {
+                if (current.children[c - 'a'] == null) {
+                    current.children[c - 'a'] = new Node();
+                }
+                current = current.children[c - 'a'];
+            }
+
+            current.word = word;
+        }
+
+        public boolean search(String word) {
+            Node current = root;
+            for (char c : word.toCharArray()) {
+                if (current.children[c - 'a'] == null) {
+                    return false;
+                }
+
+                current = current.children[c - 'a'];
+            }
+
+            return current.word.equals(word);
+        }
+    }
+}
