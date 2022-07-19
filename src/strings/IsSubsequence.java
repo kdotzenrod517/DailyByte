f69de9f60d94e18a5d512adf71bb3c1bb5776b97
Krista Dotzenrod <kdotzenrod517@gmail.com>
2022-07-18 19:01:03 -0500

    Add comment

--- src/strings/IsSubsequence.java
+++ src/strings/IsSubsequence.java
@@ -8,6 +8,10 @@ public class IsSubsequence {
         System.out.println(isSubsequence("xyz", "axbyc")); // false
     }
 
+    // Runtime: O(N) where N is the length of t. This is because in the worst case,
+    // we’ll have to iterate through all characters in t only to determine that s is not a subsequence of t.
+    // Space complexity: O(1) or constant as we only need our index variable to solve our
+    // problem regardless of the size of s and t.
     public static boolean isSubsequence(String s, String t){
 
         int index = 0;
