190584b75802002d57e5e71e699249674b870bd2
Krista Dotzenrod <kdotzenrod517@gmail.com>
2022-11-01 11:42:18 -0500

    Theft DP

---
+++ src/dynamicProgramming/Theft.java
@@ -0,0 +1,23 @@
+package dynamicProgramming;
+
+public class Theft {
+
+    public static void main(String[] args){
+        System.out.println(theft(new int[]{1, 3, 2, 5, 2})); // 8
+    }
+
+    // Runtime: O(N) where N is the total number of elements in nums.
+    // Space complexity: O(N) where N is the total number of elements in nums.
+    public static int theft(int[] nums){
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+        if(nums.length == 2) return Math.max(nums[0], nums[1]);
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for(int i = 2; i < nums.length; i++){
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+        return dp[nums.length - 1];
+    }
+}
