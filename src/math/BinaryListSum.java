745d22f3bd3ce72446df0f06f5dd3caf5de458a2
Krista Dotzenrod <kdotzenrod517@gmail.com>
2023-01-10 16:17:31 -0600

    binary list

---
+++ src/math/BinaryListSum.java
@@ -0,0 +1,24 @@
+package math;
+
+import linkedlists.MergeTwoSortedLists;
+import utils.Node;
+
+import static linkedlists.MergeTwoSortedLists.*;
+
+public class BinaryListSum {
+
+   // 1 -> 0 -> 0 -> 1, return 9 (1001 in decimal is 9).
+    // 0 -> 0 -> 1, return 1.
+
+    // Runtime: O(N) where N is the total number of elements in our list.
+    // Space complexity: O(1) or constant.
+    public static int binaryListValue(ListNode head) {
+        int value = 0;
+        while (head != null) {
+            value = value * 2 + head.val;
+            head = head.next;
+        }
+
+        return value;
+    }
+}
