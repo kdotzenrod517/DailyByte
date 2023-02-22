09d15765fae5e7ff7f53d34eab94e9493e67ade5
Krista Dotzenrod <kdotzenrod517@gmail.com>
2023-02-22 16:25:50 -0600

    Odd Numbers

---
+++ src/math/OddNumbers.java
@@ -0,0 +1,27 @@
+package math;
+
+public class OddNumbers {
+
+    public static void main(String[] args) {
+        System.out.println(oddNumbers(1, 3)); // 2 (1 and 3 are both odd)
+        System.out.println(oddNumbers(1, 10)); // 5
+    }
+
+    // O(n)
+    public static int oddNumbers(int lo, int high){
+        int sum = 0;
+        for(int i = lo; i <= high; i++){
+            if(i % 2 != 0){
+                sum++;
+            }
+        }
+
+        return sum;
+    }
+
+    // Runtime: O(1) or constant.
+    // Space complexity: O(1) or constant.
+    public int oddNumbersConstant(int low, int high) {
+        return (high + 1) / 2 - low / 2;
+    }
+}
