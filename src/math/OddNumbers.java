3aef72daf2fc5208e3b63b384cbe060804cca223
Krista Dotzenrod <kdotzenrod517@gmail.com>
2023-02-22 16:35:46 -0600

    Odd numbers

--- src/math/OddNumbers.java
+++ src/math/OddNumbers.java
@@ -2,6 +2,7 @@ package math;
 
 public class OddNumbers {
 
+    // Given two non-negative integers low and high, return the total count of odd numbers between them (inclusive).
     public static void main(String[] args) {
         System.out.println(oddNumbers(1, 3)); // 2 (1 and 3 are both odd)
         System.out.println(oddNumbers(1, 10)); // 5
@@ -15,9 +16,17 @@ public class OddNumbers {
                 sum++;
             }
         }
-
         return sum;
     }
+
+
+
+
+
+
+
+
+
 
     // Runtime: O(1) or constant.
     // Space complexity: O(1) or constant.
